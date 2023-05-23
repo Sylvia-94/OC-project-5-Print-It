db9fc2943eda4048265f6313613039c9542a5892
Sylvia Kevorkian <sylvia.kevorkian@outlook.fr>
2023-05-23 11:20:11 +0200

    Adding working arrows of the slider

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,3 +1,5 @@
+
+
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,3 +18,57 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+// Début de mon code 
+// -----------------------------------------------------------
+
+// Définition de mes variables 
+
+
+let slidesIndex = 0;
+const banner = document.querySelector(".banner");
+const bannerImg = document.querySelector(".banner-img");
+const bannerTag = document.querySelector("#tagLine");
+
+const arrowLeft = document.getElementById("arrowL");
+const arrowRight = document.getElementById("arrowR");
+
+function slideChange () {
+
+	bannerImg.src="./assets/images/slideshow/" + slides[slidesIndex].image;
+	bannerTag.innerHTML = slides[slidesIndex].tagLine;
+	
+}
+
+arrowLeft.addEventListener("click", function() {
+
+// bannerImg = récupération de la source des images, définition de la première slide à 0 par slideIndex
+// bannerTag = on définit la tagline selon la slide sur laquelle on se trouve 
+// slidesIndex -- = on enlève 1 à slideIndex
+slidesIndex--;
+
+if(slidesIndex < 0){
+
+	slidesIndex = slides.length-1;
+}
+
+slideChange();
+
+})
+
+arrowRight.addEventListener("click", function() {
+
+	// Même chose que pour flèche de gauche sauf qu'on a slidesIndex ++, donc on ajoute 1
+	slidesIndex++;
+
+	if(slidesIndex >= slides.length){
+
+		slidesIndex = 0;
+	}
+
+	slideChange();
+	
+	
+})
+
